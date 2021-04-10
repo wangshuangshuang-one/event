08d27a7939bf4157bbc9e478e0781c51ac9c5492
wangshuangshuang <1186020802@qq.com>
2021-04-10 19:51:48 +0800

    修改

--- assets/js/login.js
+++ assets/js/login.js
@@ -7,4 +7,36 @@ $(function () {
         $(".reg-box").hide();
         $(".login-box").show();
     });
+
+    // 定义校验规则
+    const form = layui.form;
+    form.verify({
+        pwd: [/^[\S]{6,12}$/, '密码必须为6-12位，且不能包含字符'],
+
+        repwd: function (value) {
+            const pwd = $('.layui-form name=password').val();
+            if (value !== pwd) {
+                alert('两次输入的密码不一致');
+            }
+        }
+    });
+
+    $('#form_reg').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            type: 'POST',
+            url: '//api-breakingnews-web.itheima.net/api/reguser',
+            data: {
+                username: $('#form_reg [name=username]').val(),
+                password: $('#form_reg [name=password]').val()
+            },
+            success(res) {
+                if (res.status !== 0) {
+                    return console.log(res.message);
+                }
+                console.log('注册成功');
+            }
+        });
+    });
+
 });

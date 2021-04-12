c50f7c9ef726ba4e84f2d3126fe5ae8c2133722d
wangshuangshuang <1186020802@qq.com>
2021-04-12 16:00:26 +0800

    16:00提交

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,14 @@
 $(function () {
-    getUserInfo()
+    getUserInfo();
+    let layer = layui.layer;
+    $("#btn_logOut").on('click', function () {
+        layer.confirm('确认退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+            localStorage.removeItem('token');
+            location.href = "/login.html";
+            layer.close(index);
+        });
+    });
 });
 
 //获取用户基本信息

0619503f5ee1dbd3ca6be5cc536ccd0d4389d7e8
wangshuangshuang <1186020802@qq.com>
2021-04-15 18:30:00 +0800

    大事件CV结束

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -24,7 +24,7 @@ $(function () {
   // 需要将请求参数对象提交到服务器
   var q = {
     pagenum: 1, // 页码值，默认请求第一页的数据
-    pagesize: 10, // 每页显示几条数据，默认每页显示2条
+    pagesize: 2, // 每页显示几条数据，默认每页显示2条
     cate_id: '', // 文章分类的 Id
     state: '' // 文章的发布状态
   }
@@ -37,7 +37,6 @@ $(function () {
       url: '/my/article/list',
       data: q,
       success: function (res) {
-        console.log(res);
         if (res.status !== 0) {
           return layer.msg('获取文章列表失败！')
         };
@@ -55,11 +54,14 @@ $(function () {
       count: total, // 总数据条数
       limit: q.pagesize, // 每页显示几条数据
       curr: q.pagenum, // 设置默认被选中的分页
+      layout: ['count', 'limit', 'prev', 'page', 'next', 'skip'],
+      limits: [2, 3, 5, 10, 15],
       // 分页发生切换的时候，触发 jump 回调
       jump: function (obj, first) {
         // console.log(obj.curr)
         // 把最新的页码值，赋值到 q 这个查询参数对象中
         q.pagenum = obj.curr;
+        q.pagesize = obj.limit;
         if (!first) {
           initTable();
         }
@@ -90,5 +92,29 @@ $(function () {
     q.cate_id = cate_id;
     q.state = state;
     initTable();
+  });
+
+  $('tbody').on('click', '.btn-delete', function () {
+    let len = $(".btn-delete").length;
+    console.log(len);
+    const id = $(this).attr('data-id');
+    layer.confirm('确认删除?', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      $.ajax({
+        type: 'GET',
+        url: '/my/article/delete/' + id,
+        success(res) {
+          if (res.status !== 0) {
+            return layer.msg("删除失败！")
+          }
+          layer.msg("删除成功！");
+          if (len === 1) {
+            q.pagenum = q.pagenum === 1 ? 1 : q.pagenum - 1;
+          }
+          initTable();
+        }
+      });
+      layer.close(index);
+    });
   });
 });
